4afe207d8a0e8125829bce256b07c2d3702f9cba
“Adam <2915142d@student.gla.ac.uk>
2026-02-13 14:05:27 +0000

    chapter 10 tests passed

--- rango/views.py
+++ rango/views.py
@@ -14,6 +14,8 @@ from django.contrib.auth import authenticate, login
 from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth.decorators import login_required
 
+from datetime import datetime
+
 def index(request):
     
     category_list = Category.objects.order_by('-likes')[:5]
@@ -22,12 +24,24 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
+    
+    visitor_cookie_handler(request)
 
-    return render(request, 'rango/index.html', context = context_dict)
+    response = render(request, 'rango/index.html', context = context_dict)
+    return response
 
 def about(request):
     
     context_dict = {'boldmessage': 'This tutorial has been put together by Adam D'}
+
+
+    if request.session.test_cookie_worked(): 
+        print("TEST COOKIE WORKED!") 
+        request.session.delete_test_cookie()
+
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+        
     return render(request, 'rango/about.html', context = context_dict)
 
 def show_category(request, category_name_slug):
@@ -151,6 +165,29 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+
+def visitor_cookie_handler(request):
+    visits = request.session.get('visits', 1)
+
+    last_visit_cookie = request.session.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
+
+
+
+def get_server_side_cookie(request, cookie, default_val=None): 
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val 
+        return val
+
 
 
 
