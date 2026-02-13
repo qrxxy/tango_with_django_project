eb279c1a3181502ae97da122e5110292314ee1bd
“Adam <2915142d@student.gla.ac.uk>
2026-02-13 02:09:06 +0000

    chapter 9 tests passed

--- rango/admin.py
+++ rango/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from rango.models import UserProfile
 
 # Register your models here.
 from django.contrib import admin
@@ -12,6 +13,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
 
 
 
